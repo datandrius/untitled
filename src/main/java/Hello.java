c16b3c12c4d463f62f33694f1ac32be95ccee4f1
datandrius <andrius.p@datadog.org>
2018-06-12 13:57:26 +0300

    sixth line

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -6,5 +6,6 @@ public class Hello {
     System.out.println("Third line");
     System.out.println("Fourth line");
     System.out.println("Fifth line");
+    System.out.println("Sixth line");
   }
 }
