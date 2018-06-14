14fd6933658846e5cea4c3358592870843073022
Andrius Popliauskas <39299671+datandrius@users.noreply.github.com>
2018-06-14 10:55:39 +0300

    first line changed on github

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 
   public static void main(String... args) {
-    System.out.println("Halissimo +onhub");
+    System.out.println("First line on github");
     System.out.println("Second line");
     System.out.println("Third line");
     System.out.println("Third line v2");
