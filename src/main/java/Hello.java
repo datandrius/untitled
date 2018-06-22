4c0eba600ce6de9be37b1200bfdbe9f616508abf
Andrius Popliauskas <39299671+datandrius@users.noreply.github.com>
2018-06-22 15:31:00 +0300

    Few sout lines up to eigtht deleted from beginning

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,10 +1,6 @@
 public class Hello {
 
   public static void main(String... args) {
-    System.out.println("Fourth line");
-    System.out.println("Fifth line");
-    System.out.println("Sixth line");
-    System.out.println("seventh line");
     System.out.println("eighth line");
     System.out.println("feature line 1");
     System.out.println("feature line 2 version2");
