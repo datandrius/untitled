e6cb3f2797b5f401914081dcb3ef3ad2ef7c9701
datandrius <andrius.p@datadog.org>
2018-06-22 14:59:28 +0300

    Stringer tests added to StringerTest.java file

--- src/test/java/StringerTest.java
+++ src/test/java/StringerTest.java
@@ -1,8 +1,34 @@
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+import java.lang.reflect.Method;
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  * @author Satalia Team.
  */
 public class StringerTest {
+
+  private Stringer stringer = new Stringer();
+  private Method m;
+
+  @Before
+  public void setUp() throws Exception {
+    m = stringer.getClass().getDeclaredMethod("concatMinusLines", String.class, String.class);
+    m.setAccessible(true);
+  }
+
+  @Test
+  public void privateMethod() throws Exception {
+    assertThat(m.invoke(stringer, "vienas", "du"), is("vienas - du"));
+  }
+
+  @Test
+  public void publicMethod() {
+    assertEquals("vienas - du", new Stringer().concatLines("vienas", "du"));
+  }
+
 
 }
