420c03d5fea9e4025ceecc1b2791bdcbea9052db
datandrius <andrius.p@datadog.org>
2018-06-22 17:47:53 +0300

    commit1 pc

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,6 +2,6 @@ public class Hello {
 
   public static void main(String... args) {
     System.out.println("feature line 2 version2");
-    System.out.println(new Stringer().concatLines("pirma", "antra"));
+    System.out.println(new Stringer().concatLines("1", "1"));
   }
 }
