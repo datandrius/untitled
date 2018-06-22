704e3d7de65f704dbc17879a8a353aa4b71b8a99
datandrius <andrius.p@datadog.org>
2018-06-22 17:53:28 +0300

    commit4 pc

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     System.out.println("feature line 2 version2");
     System.out.println(new Stringer().concatLines("1", "1"));
     System.out.println(new Stringer().concatLines("2", "2"));
+    System.out.println(new Stringer().concatLines("4", "4"));
   }
 }
